0d1d02d42d83648c19deeefe4a3bf67580369ab1
Max Tarlov <maxtarlov@gmail.com>
2023-01-05 23:16:08 -0800

    factor out renderResume

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,10 +1,33 @@
-$(document).ready(async function() { 
-    let processor = new XSLTProcessor;
-    const XSLstylesheet = await $.get('assets/data/resume.xsl');
-    processor.importStylesheet(XSLstylesheet);
-    const XMLdata = await $.get("/assets/data/resume.xml");
-    HTMLresume = processor.transformToDocument(XMLdata).querySelector("#resume-body");
-    if(HTMLresume) {
-        $("#resume-redirect").replaceWith(HTMLresume);
+const processor = new XSLTProcessor;
+
+async function renderResume(XMLData) {
+    HTMLResume = processor.transformToDocument(XMLData)
+    .getElementById("resume-body");
+    
+    if(!(HTMLResume instanceof HTMLElement)) {
+        throw new Error("Failed to process XML with XSLTProcessor");
+    } 
+
+    if($("#resume-redirect").length) {
+        $("#resume-redirect").replaceWith(HTMLResume);
     }
+    else if($("#resume-body").length) {
+        $("#resume-body").replaceWith(HTMLResume);
+    }
+    else {
+        throw new Error("Failed to find #resume-redirect or #resume-body")
+    }
+}
+
+$(document).ready(async function() { 
+    const XMLData = await $.get("/assets/data/resume.xml").fail(function() {
+        console.error("Failed to fetch XML resume")
+    });
+    $.get('assets/data/resume.xsl', function(data) {
+        processor.importStylesheet(data);
+    }).done(function() {
+        renderResume(XMLData);
+    }).fail(function() {
+        console.error("Failed to fetch XSL stylesheet");
+    });
 });
